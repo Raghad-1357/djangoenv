c99d98b4530c53443de29f6e503771b001bd49dc
Pixel <pixel@webmail.com>
2026-02-16 14:05:00 +0300

    first commit

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+# import apps.bookmodule.views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', apps.bookmodule.views.index),  #add only this line of code
+#     path('index2/<int:val1>/', apps.bookmodule.views.index2)  #add only this line
+# ]
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', include("apps.bookmodule.urls")), #include urls.py of bookmodule app
+    path('users/', include("apps.usermodule.urls"))  #include urls.py of usermodule app
 ]
